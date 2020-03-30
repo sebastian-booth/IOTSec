41230606b6f797b4535d39e6c2043f4f65c3e1e9
sbooth <csc@cscIOTsec.admin>
2020-03-30 01:13:39 +0100

    fix

---
+++ tcp_sniffer.py
@@ -0,0 +1,23 @@
+import pyshark  # http://kiminewt.github.io/pyshark/
+import json
+import random
+from zxcvbn import zxcvbn
+
+# change interface and src ip as needed
+
+def main():
+    capture = pyshark.LiveCapture(interface='\Device\\NPF_{2F1DBFFB-5D87-4B88-BA83-CF0104043E2E}',display_filter='ip.src == 192.168.1.146')
+    capture.sniff(timeout=10)
+    print (capture)
+    while True:
+        try:
+            p = capture.next()
+        except StopIteration:
+            break
+        try:
+            print (p.data.data.binary_value)
+        except AttributeError:
+            pass
+
+
+main()
